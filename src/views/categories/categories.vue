2c941517f032fcde6638a071a13d5a252ce55859
FinGet <zhangbiaoFT@163.com>
2018-11-13 16:00:17 +0800

    用户管理

--- src/views/categories/categories.vue
+++ src/views/categories/categories.vue
@@ -1,12 +1,84 @@
 <template>
   <div id="category" class="clearfix">
-    <div class="page-title">品类管理</div>
+    <div class="page-top clearfix">
+      <div class="page-title left">品类管理</div>
+      <el-button type="primary" @click="handleAdd" class="right"><i class="fa fa-plus"></i>添加品类</el-button>
+    </div>
+    <div class="table-container">
+      <el-table
+        :data="firstCategory"
+        border
+        stripe
+        v-loading="loading"
+        style="width: 100%">
+        <el-table-column
+          label="品类id"
+          prop="id"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          label="品类名称"
+          prop="name"
+          >
+        </el-table-column>
+        <el-table-column label="操作" width="220">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">修改名称</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleChild(scope.$index, scope.row)">查看子类</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'category',
+  data() {
+    return {
+      loading: false,
+      firstCategory: []
+    }
+  },
+  created() {
+    this.getCategory()
+  },
+  methods: {
+    /**
+     * 获取品类 一级品类不需要id ，二级品类需要 父级id
+     */
+    getCategory(id) {
+      this.loading = true
+      this.$http.get('/api/manage/category/get_category.do',{
+        params: {
+          categoryId:  id || 0
+        }
+      }).then(response => {
+        let res = response.data
+        this.loading = false
+        if(res.status == 0) {
+          if (!id) {
+            // 一级分类
+            this.firstCategory = res.data
+          } else {
+            // 二级分类
+            this.secondCategory = res.data
+          }
+        }
+        
+      })
+    },
+    // 点击新增品类
+    handleAdd() {
+
+    }
+  }
 }
 </script>
 
--- src/views/users/users.vue
+++ src/views/users/users.vue
@@ -1,12 +1,95 @@
 <template>
   <div id="user" class="clearfix">
     <div class="page-title">用户管理</div>
+    <div class="table-container">
+      <el-table
+        :data="users"
+        border
+        stripe
+        v-loading="loading"
+        style="width: 100%">
+        <el-table-column
+          label="ID"
+          prop="id"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          label="用户名"
+          prop="username"
+          >
+        </el-table-column>
+        <el-table-column
+          label="邮箱"
+          prop="email"
+          >
+        </el-table-column>
+        <el-table-column
+          label="电话"
+          prop="phone"
+          >
+        </el-table-column>
+        <el-table-column
+          label="注册时间"
+          prop="createTime"
+          >
+        </el-table-column>
+      </el-table>
+    </div>
+    <el-pagination
+      v-if="total>pageSize"
+      class="pagination"
+      background
+      layout="prev, pager, next"
+      @current-change="changPage"
+      :page-size="pageSize"
+      :total="total">
+    </el-pagination>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'User',
+  data() {
+    return {
+      users: [],
+      pageNum: 1,
+      pageSize: 12,
+      loading: false,
+      total: 0
+    }
+  },
+  created() {
+    this.getUsers()
+  },
+  methods: {
+    getUsers() {
+      this.loading = true
+      this.$http.get('/api/manage/user/list.do',{
+        params: {
+          pageSize: this.pageSize,
+          pageNum: this.pageNum
+        }
+      }).then(response => {
+        let res = response.data
+        res.data.list.forEach(item => {
+          item.createTime = new Date(item.createTime).toLocaleString()
+        })
+        if(res.status == 0) {
+          this.total = res.data.total
+          this.loading = false
+          this.users = res.data.list
+        }
+      })
+    },
+    /**
+     * 分页
+     */
+    changPage(val){
+      this.pageNum = val
+      this.getUsers()
+    },
+  }
 }
 </script>
 
